38aadc314c4faaf336426bbb585b0cc5dff3adbd
Morten Rasmussen <morten.rasmussen@arm.com>
2014-07-03 16:25:49 +0800

    sched: Make energy awareness a sched feature
    This patch introduces the ENERGY_AWARE sched feature, which is
    implemented using jump labels when SCHED_DEBUG is defined. It is
    statically set false when SCHED_DEBUG is not defined. Hence this doesn't
    allow energy awareness to be enabled without SCHED_DEBUG. This
    sched_feature knob will be replaced later with a more appropriate
    control knob when things have matured a bit.
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4462,6 +4462,11 @@ static long effective_load(struct task_group *tg, int cpu, long wl, long wg)
 
 #endif
 
+static inline bool energy_aware(void)
+{
+	return sched_feat(ENERGY_AWARE);
+}
+
 static int wake_wide(struct task_struct *p)
 {
 	int factor = this_cpu_read(sd_llc_size);
