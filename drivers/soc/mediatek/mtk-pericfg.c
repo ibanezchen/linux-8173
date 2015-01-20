8607fddec7776cf793c6fdcf2e0bf6845d9efaba
Sascha Hauer <s.hauer@pengutronix.de>
2015-01-20 10:47:05 +0100

    soc: Add MediaTek pericfg controller support
    This adds support for the MediaTek pericfg controller found
    on the MT8135/MT8173 SoCs. The pericfg controller contains
    miscellaneous registers for controlling peripheral resets and
    clocks.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- drivers/soc/mediatek/mtk-pericfg.c
+++ drivers/soc/mediatek/mtk-pericfg.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2014 MediaTek Inc.
- * Author: Flora Fu <flora.fu@mediatek.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
